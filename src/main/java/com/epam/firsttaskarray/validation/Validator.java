6d0206c337a8b7635ba10169aab0924f448ca3df
Ivanukovich <47699006+Ivanukovich@users.noreply.github.com>
2021-10-05 11:00:39 +0300

    Update Validator.java

--- src/main/java/com/epam/firsttaskarray/validation/Validator.java
+++ src/main/java/com/epam/firsttaskarray/validation/Validator.java
@@ -4,6 +4,6 @@ public class Validator {
     private final static String regex_integer = "^-?\\d+(\\s(-?\\d+))*$";
 
     public boolean validateLine(String line){
-        return line.matches("^-?\\d+(\\s(-?\\d+))*$");
+        return line.matches(regex_integer);
     }
 }
